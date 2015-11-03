8b48130ce100696e94497a8701cf06ed137b4ec8
Damien Gavard <damien@viscaweb.com>
2015-11-03 23:00:28 +0100

    Add base file

---
+++ DAG/Bundle/AliceExtraBundle/DAGAliceExtraBundle.php
@@ -0,0 +1,11 @@
+<?php
+namespace DAG\Bundle\AliceExtraBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DAGAliceExtraBundle
+ */
+class DAGAliceExtraBundle extends Bundle
+{
+}
